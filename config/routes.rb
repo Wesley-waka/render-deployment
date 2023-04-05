a52c4c8deab1edb7325297d2199dad1e0feb95e2
Wesley-waka <wesley.waka@student.moringaschool.com>
2023-04-05 10:33:16 +0300

    Done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :birds
+  get '/birds/:id', to: 'birds#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "birds#index"
   # Defines the root path route ("/")
